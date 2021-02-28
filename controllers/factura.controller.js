63d762484bd98386baff2fa48b32baddf029bde3
Joshua Navarro <joshua.navarro35@gmail.com>
2021-02-27 20:10:04 -0500

    Actualizar recibo al hacer un pago

--- controllers/factura.controller.js
+++ controllers/factura.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Factura = require("..//models//factura.model");
+const Recibo = require("../models/recibo.model");
 
 const createFactura = async (req, res) => {
   try {
@@ -10,6 +11,7 @@ const createFactura = async (req, res) => {
       return res.status(400).json({ mensaje: "ID invalido" });
     }
     const nuevaFactura = await Factura.create({ cuenta_id, recibo_id });
+    await Recibo.updateOne({ _id: recibo_id }, { pagado: true });
 
     res.status(201).json(nuevaFactura);
   } catch (error) {
